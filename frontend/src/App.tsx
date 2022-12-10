27016c9731d5e2a9d94a3de37e917b9bdb86f340
timsmr <smrtim17@gmail.com>
2022-12-10 11:58:26 +0300

    added Profile page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,7 @@ import { useStore } from "./stores";
 import { useLayoutEffect } from "react";
 import { PrivateRoute } from "shared/components/PrivateRoute";
 import { Feed } from "pages/Feed";
+import { Profile } from "pages/Profile";
 
 
 const App = () => {
@@ -24,6 +25,7 @@ const App = () => {
     <div className='wrapper'>
       <Routes>
         <Route path="/feed" element={<Feed />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="/auth/:authType" element={<Auth />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
---
+++ frontend/src/pages/Profile/index.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import Button from 'shared/components/Button'
+import InputField from 'shared/components/InputField'
+import { Tag } from 'shared/components/Tag'
+import Textarea from 'shared/components/Textarea'
+import { ToggleButton } from 'shared/components/ToggleButton'
+
+import styles from './index.module.scss'
+
+import * as I from './types/types'
+
+export const Profile = ({ }: I.ProfileProps) => {
+
+    const options = [
+        { 'my_data': 'Личные данные' },
+        { 'responses': 'Отклики' },
+        { 'my_resumes': 'Мои резюме' },
+        { 'my_projects': 'Мои проекты' },
+    ]
+
+    return (
+        <>
+            <ToggleButton className={styles.toggleButton} options={options} />
+
+            <ul className={styles.my_data}>
+                <li className={styles.data_line}>
+                    Имя <br /><br />
+                    <InputField className={styles.inputField} inputPlaceholder='Имя' />
+                </li>
+                <li className={styles.data_line}>
+                    Фамилия <br /><br />
+                    <InputField className={styles.inputField} inputPlaceholder='Фамилия' />
+                </li>
+                <li className={styles.data_line}>
+                    Специализация <br /><br />
+                    <InputField className={styles.inputField} inputPlaceholder='Специализация' />
+                </li>
+                <li className={styles.data_line}>
+                    Образование <br /><br />
+                    <Textarea className={styles.inputField} textareaPlaceholder='Образование' />
+                </li>
+                <li className={styles.data_line}>
+                    Hard & soft skills <br /><br />
+                    <Textarea className={styles.inputField} textareaPlaceholder='Hard & soft skills' />
+                </li>
+                <li className={styles.data_line}>
+                    Участие в проектах <br /><br />
+                    <Textarea className={styles.inputField} textareaPlaceholder='Участие в проектах' />
+                </li>
+                <li className={styles.data_line}>
+                    О себе <br /><br />
+                    <Textarea className={styles.inputField} textareaPlaceholder='О себе' />
+                </li>
+                <Button className={styles.save_btn} label='Сохранить' buttonStyle='primary' />
+            </ul>
+        </>
+    )
+}
