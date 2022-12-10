c173a72e06f8aa95f96f54f18fe80133223576c6
Kremon <kremoor@gmail.com>
2022-12-10 19:25:17 +0300

    db migration

--- backend/alembic_/versions/c7eb1f4caaab_init.py
+++ backend/alembic_/versions/c7eb1f4caaab_init.py
@@ -1,8 +1,8 @@
 """init
 
-Revision ID: 7faac742d5ae
+Revision ID: c7eb1f4caaab
 Revises: 
-Create Date: 2022-12-10 19:22:02.295365
+Create Date: 2022-12-10 19:24:56.102360
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '7faac742d5ae'
+revision = 'c7eb1f4caaab'
 down_revision = None
 branch_labels = None
 depends_on = None
