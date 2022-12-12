cc4b4535c0e8d0ed6108ccd0963ced4824b8040b
Kremon <kremoor@gmail.com>
2022-12-12 14:38:01 +0300

    nullable imporevemts

--- backend/app/routers/organization_router/organization_models.py
+++ backend/app/routers/organization_router/organization_models.py
@@ -4,9 +4,9 @@ from pydantic import BaseModel
 class OrganizationChangeInfoModel(BaseModel):
     email: str
     name: str
-    description: str
-    contacts: str
-    specialization: str
+    description: str | None
+    contacts: str | None
+    specialization: str | None
     
     
 class OrganizationModel(OrganizationChangeInfoModel):
